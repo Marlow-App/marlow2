3fd46027f36dd7023157d561100eac81430e90d2
Marlow-App <bymarloww@gmail.com>
2026-04-15 21:36:19 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
+  root: path.resolve(__dirname, "client"),
   plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, "dist/public"),
+    emptyOutDir: true,
+  },
 });
